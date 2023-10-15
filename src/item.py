c356ac418aa0ca90b6042940b0dcc4259fdd5b00
Dmitriy Frolov <dmitriy.a.frolov@gmail.com>
2023-10-16 02:48:54 +0300

    Make the name attribute private and access them via @property, initialize an instance of the Item class via a .csv file

--- src/item.py
+++ src/item.py
@@ -1,3 +1,7 @@
+import os.path
+from csv import DictReader
+
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -15,7 +19,7 @@ class Item:
         """
         self.name = name
         self.price = price
-        self.quantiry = quantity
+        self.quantity = quantity
 
         Item.all += 1
 
@@ -25,7 +29,7 @@ class Item:
 
         :return: Общая стоимость товара.
         """
-        return self.quantiry * self.price
+        return self.quantity * self.price
 
     def apply_discount(self) -> float:
         """
@@ -33,3 +37,53 @@ class Item:
         """
         self.price *= self.pay_rate
         return self.price
+
+    @classmethod
+    def verify_name(cls, name):
+        """
+        Имя должно быть не больше 10 символов
+        """
+        return name[:10]
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, name):
+        name = self.verify_name(name)
+        self.__name = name
+
+    @classmethod
+    def instantiate_from_csv(cls, path_name):
+        """
+        Создаёт объекты из данных файла .csv
+        """
+
+        items = []
+        path_name = str(cls.path_file(path_name))
+        with open(path_name, newline='', encoding='windows-1251') as csv_f:
+            reader = DictReader(csv_f)
+            for row in reader:
+                name = str(row['name'])
+                price = float(row['price'])
+                quantity = int(row['quantity'])
+                item = cls(name, price, quantity)
+                items.append(item)
+            cls.all = items
+
+    @staticmethod
+    def path_file(path_name):
+        """
+        Создаёт путь для файла при условии, что файл лежит в другой папке родительского каталога
+
+        :param path_name: путь к файлу в подобном формате 'src/items.csv'
+        """
+        path_list = path_name.split('/')
+        path_file = os.path.join('..', path_list[0], path_list[1])
+        return path_file
+
+    @staticmethod
+    def string_to_number(string):
+        string.split('.')
+        return int(string[0])
--- tests/test_item.py
+++ tests/test_item.py
@@ -3,18 +3,45 @@ from src.item import Item
 
 def test_item():
     """Тестирование класса Item из src/item.py, служащего для представления товара в магазине"""
+
+    # Создаём для экземпляра класса для проведения проверок
     item1 = Item("Смартфон", 10000, 20)
     item2 = Item("Ноутбук", 20000, 5)
 
     assert item1.calculate_total_price() == 200000
     assert item2.calculate_total_price() == 100000
 
-    # устанавливаем новый уровень цен
+    # Устанавливаем новый уровень цен
     item1.pay_rate = 0.8
-    # применяем скидку
+    # Применяем скидку
     item1.apply_discount()
-
+    # Проверяет применилась ли цена с учётом нового уровня цен
     assert item1.price == 8000.0
     assert item2.price == 20000
 
+    # Проверяет количество созданных классов
     assert Item.all == 2
+
+    # Длина наименования товара меньше 10 символов
+    item1.name = 'Смартфон'
+    assert item1.name == 'Смартфон'
+
+    # Длина наименования товара больше 10 символов
+    item1.name = 'СуперСмартфон'
+    assert item1.name == 'СуперСмарт'
+
+
+def test_item_csv():
+    """Тестирование класса Item из src/item.py, служащего для представления товара в магазине. Получение данных из
+    файла .csv"""
+
+    # Создание объектов из данных файла
+    Item.instantiate_from_csv('src/items.csv')
+
+    # В файле 5 записей с данными по товарам
+    assert len(Item.all) == 5
+
+def test_item_string_to_number():
+    assert Item.string_to_number('5') == 5
+    assert Item.string_to_number('5.0') == 5
+    assert Item.string_to_number('5.5') == 5
