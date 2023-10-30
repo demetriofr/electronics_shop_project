a9beb870491e2835fbd66056c5f22c4c29e534f7
Dmitriy Frolov <dmitriy.a.frolov@gmail.com>
2023-10-31 00:15:48 +0300

    Add valid value checks for the number_of_sim attribute and the tests that cover them

--- src/phone.py
+++ src/phone.py
@@ -5,10 +5,30 @@ class Phone(Item):
     """
     Класс для представления конкретного товара телефон в магазине
     """
+
     def __init__(self, name, price, quantity, number_of_sim):
         super().__init__(name, price, quantity)
         # количество поддерживаемых сим-карт
         self.number_of_sim = number_of_sim
+
+    @classmethod
+    def verify_number_of_sim(cls, number_of_sim):
+        """
+        Проверяет чтобы имя было целым числом и больше нуля
+        """
+        if type(number_of_sim) == int and number_of_sim > 0:
+            return number_of_sim
+        else:
+            raise ValueError('Количество физических SIM-карт должно быть целым числом больше нуля.')
+
+    @property
+    def number_of_sim(self):
+        return self.__number_of_sim
+
+    @number_of_sim.setter
+    def number_of_sim(self, number_of_sim):
+        number_of_sim = self.verify_number_of_sim(number_of_sim)
+        self.__number_of_sim = number_of_sim
 
     def __repr__(self):
         """
--- tests/test_phone.py
+++ tests/test_phone.py
@@ -1,4 +1,4 @@
-import pytest
+from pytest import raises
 
 from src.phone import Phone
 from src.item import Item
@@ -12,10 +12,32 @@ def test_class_phone():
     assert phone1.number_of_sim == 2
 
     item1 = Item("Смартфон", 1000, 20)
-    phone1.number_of_sim = 0
+
     assert item1 + phone1 == 25
     assert phone1 + phone1 == 10
 
     # Проверка невозможности сложения не экземпляров классов Phone или Item
-    with pytest.raises(ValueError):
+    with raises(ValueError):
         phone1 + 10
+
+
+def test_class_phone__number_of_sim():
+    phone1 = Phone("iPhone 14", 120000, 5, 2)
+
+    assert phone1.number_of_sim == 2
+
+    # Число сим-карт не может быть равным нулю
+    with raises(ValueError):
+        phone1.number_of_sim = 0
+
+    # Число сим-карт не может быть равным отрицательному числу
+    with raises(ValueError):
+        phone1.number_of_sim = -1
+
+    # Число сим-карт не может быть равным вещественному числу
+    with raises(ValueError):
+        phone1.number_of_sim = 1.0
+
+    # Число сим-карт не может быть равным строковому значению
+    with raises(ValueError):
+        phone1.number_of_sim = "1"
